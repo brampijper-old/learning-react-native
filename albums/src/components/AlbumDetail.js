cb1e9b8b9ffeb00a0470068dc927f33b05a46f35
bram <brampijper@gmail.com>
2018-01-15 22:08:29 +0100

    added an album-image to the album page and added overall styling

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,16 +1,53 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card.js';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+    const { title, artist, thumbnail_image } = album;
+    const { 
+        thumbnailStyle, 
+        headerContentStyles, 
+        thumbnailContainerStyle,
+        headerTextStyle 
+    } = styles; 
+
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View style={thumbnailContainerStyle}>
+                    <Image 
+                        style={thumbnailStyle} 
+                        source={{ uri: thumbnail_image }} 
+                    />
+                </View>
+                <View style={headerContentStyles}>
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
             </CardSection>
         </Card>
     );
+};
+
+const styles = {
+    headerContentStyles: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18,
+    }, 
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10, 
+        marginRight: 10
+    }
 };
 
 export default AlbumDetail;
