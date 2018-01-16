7cee4b11276457e5ce3231d69fa342c422a082c0
bram <brampijper@gmail.com>
2018-01-16 11:29:34 +0100

    Set-up a basic re-usable button component

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card.js';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
     const { title, artist, thumbnail_image, image } = album;
@@ -33,6 +34,10 @@ const AlbumDetail = ({ album }) => {
                     style={imageStyle}
                     source={{ uri: image }} 
                 />
+            </CardSection>
+
+            <CardSection>
+                <Button />
             </CardSection>
         </Card>
     );
