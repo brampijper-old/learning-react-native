5f7247c4c47469b88a6f19eb1558e543f1dd7856
bram <brampijper@gmail.com>
2018-01-16 11:04:30 +0100

    added an extra CardSection to display album images

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -4,12 +4,13 @@ import Card from './Card.js';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image } = album;
+    const { title, artist, thumbnail_image, image } = album;
     const { 
         thumbnailStyle, 
         headerContentStyles, 
         thumbnailContainerStyle,
-        headerTextStyle 
+        headerTextStyle,
+        imageStyle 
     } = styles; 
 
     return (
@@ -25,6 +26,13 @@ const AlbumDetail = ({ album }) => {
                     <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
+            </CardSection>
+
+            <CardSection>
+                <Image
+                    style={imageStyle}
+                    source={{ uri: image }} 
+                />
             </CardSection>
         </Card>
     );
@@ -47,6 +55,11 @@ const styles = {
         alignItems: 'center',
         marginLeft: 10, 
         marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 };
 
