49c9a0a37aa425992d27fad81371e57439d13ca1
bram <brampijper@gmail.com>
2018-01-17 11:10:44 +0100

    added firebase initializing code

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { Header } from './components/common';  
+import firebase from 'firebase';   
+import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyB9ygNf-EWLCJb_EbjO8L5-inxRuZRmF0Y',
+            authDomain: 'authenticator-b3162.firebaseapp.com',
+            databaseURL: 'https://authenticator-b3162.firebaseio.com',
+            projectId: 'authenticator-b3162',
+            storageBucket: 'authenticator-b3162.appspot.com',
+            messagingSenderId: '921021726389'
+        }); 
+    }
+
     render() {
         return (
             <View>
-                <Header headerText="Authentication" /> 
+                <Header headerText='Authentication' /> 
                 <Text>Second App!</Text>
             </View>
         );
