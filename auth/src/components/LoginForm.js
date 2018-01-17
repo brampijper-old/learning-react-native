6ba5ee11cd638ffdad8d9374522a2260ac4b0e95
bram <brampijper@gmail.com>
2018-01-17 16:20:00 +0100

    Added basic LoginForm component so users can log-in

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Field } from './common';
 
 class LoginForm extends Component {
+    state = { email: '' };
+
     render() {
         return (
             <Card>
+                <CardSection>
+                    <Field
+                        capitalize="none"
+                        placeholder="user@gmail.com"
+                        label="Email:"
+                        onChangeText={email => this.setState({ email })}
+                    />
+                </CardSection>
                 <CardSection />
-                <CardSection />
+
                 <CardSection>
                     <Button>
                         Log in
