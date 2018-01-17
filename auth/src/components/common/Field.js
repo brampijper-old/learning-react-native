9e57690ad25abb39d9d947f47b881826d31f2cc9
bram <brampijper@gmail.com>
2018-01-17 16:23:26 +0100

    added the re-usable field component

---
+++ auth/src/components/common/Field.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {TextInput, View, Text } from 'react-native';
+
+const Field = ({ label, onChangeText, placeholder, capitalize }) => {
+    const { fieldStyle, labelStyle, containerStyle } = styles; 
+
+    return (
+        <View style={containerStyle}>
+            <Text style={labelStyle}>{label}</Text>
+            <TextInput
+                autoCapitalize={capitalize}
+                placeholder={placeholder}
+                autoCorrect={false}
+                onChangeText={onChangeText}
+                style={fieldStyle}
+            />
+        </View>
+    );
+};
+
+const styles = {
+    fieldStyle: {
+        color: '#000',
+        paddingRight: 5, 
+        paddingLeft: 5,
+        fontSize: 18,
+        lineHeight: 23,
+        flex: 2,
+        height: 20,
+        width: 100
+    },
+    labelStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+        flex: 1
+    },
+    containerStyle: {
+        height: 40,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
+};
+
+export { Field };
+
