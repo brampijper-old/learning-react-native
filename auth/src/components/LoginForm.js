039046f92107affb403588cacfebae635854196f
bram <brampijper@gmail.com>
2018-01-18 22:10:45 +0100

    added a fail and succes function when a user tries to login

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -12,12 +12,25 @@ class LoginForm extends Component {
         this.setState({ error: '', loading: true }); 
 
         firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(this.onLoginSucces.bind(this))
             .catch(() => {
                 firebase.auth().createUserWithEmailAndPassword(email, password) 
-                    .catch(() => {
-                        this.setState({ error: 'Authentication Failed.' }); 
-                    });
+                    .then(this.onLoginSucces.bind(this))
+                    .catch(this.onLoginFail.bind(this)); 
             });
+    }
+
+    onLoginFail() {
+        this.setState({ error: 'Authentication Failed', loading: false }); 
+    }
+
+    onLoginSucces() {
+        this.setState({
+            email: '',
+            password: '',
+            loading: false,
+            erorr: ''
+        });
     }
 
     renderButton() {
