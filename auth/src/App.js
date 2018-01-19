3a5711d289e2bf06f014e82b234c2f95e56071c5
bram <brampijper@gmail.com>
2018-01-19 21:03:56 +0100

    Added switch statement for handling a user log-in

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';   
-import { Header } from './components/common';
+import { Header, Button, Spinner, CardSection } from './components/common';
 import LoginForm from './components/LoginForm'; 
 
 class App extends Component {
+
+    state = { loggedIn: null }; 
+
     componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyB9ygNf-EWLCJb_EbjO8L5-inxRuZRmF0Y',
@@ -13,14 +16,46 @@ class App extends Component {
             projectId: 'authenticator-b3162',
             storageBucket: 'authenticator-b3162.appspot.com',
             messagingSenderId: '921021726389'
+        });
+        
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
         }); 
     }
 
+    renderContent() {
+        switch (this.state.loggedIn) {
+            
+            case true:
+                return (
+                    <CardSection>
+                        <Button onPress={() => firebase.auth().signOut()}>
+                            Log Out
+                        </Button>
+                    </CardSection>
+                );
+            
+            case false:
+                return <LoginForm />; 
+            
+            default:
+                return (
+                    <View style={{ marginTop: 260 }}>
+                        <Spinner spinnerSize="large" />
+                    </View>
+                );
+        }
+    }
+ 
     render() {
         return (
             <View>
                 <Header headerText='Authentication' /> 
-                <LoginForm />            
+                    { this.renderContent() }          
             </View>
         );
     }
