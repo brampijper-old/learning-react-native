d73b4974f17fcad129dfe80c82d18e36a77d7724
bram <brampijper@gmail.com>
2018-01-21 18:00:36 +0100

    User has to give his current password when changing his emailadress

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import firebase from 'firebase';   
 import { Header, Button, Spinner, CardSection, Field } from './components/common';
 import LoginForm from './components/LoginForm'; 
 
 class App extends Component {
 
-    state = { loggedIn: null, email: '' }; 
+    state = { loggedIn: null, email: '', updateEmail: null, error: '', password: '' }; 
 
     componentWillMount() {
         firebase.initializeApp({
@@ -27,47 +27,85 @@ class App extends Component {
         }); 
     }
 
-    onUpdatePress() {
-        const { email } = this.state;
+    onLogOut() {
+        this.setState({ error: '' }); 
+        firebase.auth().signOut();
+    }
 
+    //Use Firebase to check if it's possible the change the user his emailadress.
+    changeEmail() {
+        const { email } = this.state;
         const user = firebase.auth().currentUser;
+        const credential = firebase.auth.EmailAuthProvider.credential(
+            user.email,
+            this.state.password
+        );
 
         user.updateEmail(email).then(() => {
-          console.log('succesfully changed email to', email);
+            user.reauthenticateWithCredential(credential).then(() => {
+                this.setState({ updateEmail: true, error: 'Email succesfully updated' }); 
+            }).catch((error) => {
+                console.log(error);
+                this.setState({ updateEmail: false, error: 'Wrong password' });
+            }); 
         }).catch((error) => {
-          console.log(error); 
+            this.setState({ error: 'Session ended, please log-in again!' }); 
+             console.log(error); 
         });
     }
 
-    renderUserInfo() {
-        const user = firebase.auth().currentUser;
+    //Shows message if user updates his emailadress.
+    showAirmsg() {
+        switch (this.state.updateEmail) {
 
-        if (user != null) {
+        case true:
             return (
-                <Field 
-                    placeholder={user.email} 
-                    label=" Your Email:" 
-                    onChangeText={email => this.setState({ email })}
-                /> 
+                    <Text> { this.state.error } </Text>
+            );
+
+        case false:
+            return (
+                <Text> { this.state.error } </Text>
+            );
+        
+        default:
+            return (
+                    <Text> { this.state.error } </Text>
             );
         }
     }
 
     renderContent() {
+        const user = firebase.auth().currentUser;
+
         switch (this.state.loggedIn) {
             
             case true:
                 return (
                     <View>
                         <CardSection>
-                            { this.renderUserInfo() }
+                            <Field 
+                                capitalize="none"
+                                placeholder={user.email} 
+                                label=" Your Email:" 
+                                onChangeText={email => this.setState({ email })}
+                            />
+                        </CardSection>
+
+                        <CardSection>
+                            <Field 
+                                secureTextEntry
+                                placeholder={'Your current psw'} 
+                                label="Password:" 
+                                onChangeText={password => this.setState({ password })}
+                            />
                         </CardSection>
                         
                         <CardSection>
-                        <Button onPress={this.onUpdatePress.bind(this)}>
+                        <Button onPress={this.changeEmail.bind(this)}>
                             Change email
                         </Button>
-                        <Button onPress={() => firebase.auth().signOut()}>
+                        <Button onPress={this.onLogOut.bind(this)}>
                             Log Out
                         </Button>
                         </CardSection>
@@ -92,7 +130,8 @@ class App extends Component {
         return (
             <View>
                 <Header headerText='Authentication' /> 
-                { this.renderContent() }          
+                { this.renderContent() }
+                { this.showAirmsg() }          
             </View>
         );
     }
