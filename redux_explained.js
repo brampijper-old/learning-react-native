a71e9296c189567c3c2401298eadb484b00d3d1d
bram <brampijper@gmail.com>
2018-01-24 11:09:07 +0100

    Added more comments

--- redux_explained.js
+++ redux_explained.js
@@ -4,16 +4,21 @@
 const reducer = (state = [], action) => {
     if (action.type === 'split_string') {
         return ation.payload.split('');
-    }  
+    } else if (action.type === 'add_character') {
+        return [ ...state, action.payload ]; 
+    }
+    
+    return state;
 };
 
-
+//Instances of a store with one reducer. 
 const store = Redux.createStore(reducer);
 
 store.getState(); 
 
 //type property is always a string - command / instruction for the reducer.
 //You want to manipulate the payload. 
+//Action -> you want to tell the reducer to update the state in a specific way. 
 const action = {
     type: 'split_string',
     payload: 'asdf'
@@ -21,4 +26,20 @@ const action = {
 
 // How to feed the action into the reducer?
 store.dispatch(action);
+
+//The state is either going to be an empty array or an array of characters. 
 store.getState();
+
+
+const action2 = {
+    type: 'add_character',
+    payload: 'a'
+};
+store.dispatch(action2); 
+
+
+
+//Why would you use Redux?
+// Redux Is on of the best libraries for scaling an application, to be very large, with the least ammount of code complexity.
+
+//You always return brand new objects from reducers.
