70cfc591b552a3f85eeca05084af3106cbae0782
bram <brampijper@gmail.com>
2018-02-03 22:57:14 +0100

    Implemented mapStateToProps method

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'; 
 import { connect } from 'react-redux';
 import { Card, CardSection, Input, Button } from './common'; 
-import { emailChanged } from '../actions/'
+import { emailChanged } from '../actions/'; 
 
 class LoginForm extends Component {
     onEmailChange(text) {
@@ -10,29 +10,38 @@ class LoginForm extends Component {
 
     render() {
         return (
-            <CardSection>
-                <Input
-                    label='Email'
-                    placeHolder='email@gmail.com'
-                    onChangeText={this.onEmailChange.bind(this)}
-                />
-            </CardSection>
+            <Card>
+                <CardSection>
+                    <Input
+                        label='Email'
+                        placeholder='email@gmail.com'
+                        onChangeText={this.onEmailChange.bind(this)}
+                        value={this.props.email}
+                    />
+                </CardSection>
 
-            <CardSection>
-                <Input 
-                    secureTextEntry
-                    label='Password'
-                    placeHolder='password'
-                />
-            </CardSection>
+                <CardSection>
+                    <Input 
+                        secureTextEntry
+                        label='Password'
+                        placeholder='password'
+                    />
+                </CardSection>
 
-            <CardSection>
-                <Button>
-                    Login
-                </Button>
-            </CardSection>
+                <CardSection>
+                    <Button>
+                        Login
+                    </Button>
+                </CardSection>
+            </Card>
         ); 
     }
 }
 
-export default connect(null, { emailChanged })(LoginForm);
+const mapStateToProps = state => {
+    return {
+        email: state.auth.email
+    };
+};
+
+export default connect(mapStateToProps, { emailChanged })(LoginForm);
