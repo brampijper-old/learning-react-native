c1e1547e80929b74ed1deee3e9c462ec5a44469e
bram <brampijper@gmail.com>
2018-02-04 17:25:21 +0100

    Created new login action

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'; 
+
 import { 
         EMAIL_CHANGED, 
         PASSWORD_CHANGED 
@@ -17,4 +19,9 @@ export const passwordChanged = (text) => {
         payload: text
     };
 };
+
+export const loginUser = ({ email, password }) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(user => console.log(user)); 
+}; 
 
