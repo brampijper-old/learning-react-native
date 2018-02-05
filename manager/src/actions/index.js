5c45bfd5fc498cf338016799b63569014b8e22a4
bram <brampijper@gmail.com>
2018-02-05 12:03:14 +0100

    implemented on login user success

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -3,7 +3,7 @@ import firebase from 'firebase';
 import { 
         EMAIL_CHANGED, 
         PASSWORD_CHANGED,
-        LOGIN_USER_SUCCES
+        LOGIN_USER_SUCCESS
 } from './types';
 
 export const emailChanged = (text) => {
@@ -25,7 +25,7 @@ export const loginUser = ({ email, password }) => {
     return (dispatch) => {
         firebase.auth().signInWithEmailAndPassword(email, password)
         .then(user => {
-            dispatch({ type: LOGIN_USER_SUCCES, payload: user }); 
+            dispatch({ type: LOGIN_USER_SUCCESS, payload: user }); 
         }); 
     };
 }; 
