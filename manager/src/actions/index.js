4a2920f42151076b231f28b4384e818c05f4d88d
bram <brampijper@gmail.com>
2018-02-05 11:55:39 +0100

    Added login_user_succes type

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 
 import { 
         EMAIL_CHANGED, 
-        PASSWORD_CHANGED 
+        PASSWORD_CHANGED,
+        LOGIN_USER_SUCCES
 } from './types';
 
 export const emailChanged = (text) => {
@@ -24,7 +25,7 @@ export const loginUser = ({ email, password }) => {
     return (dispatch) => {
         firebase.auth().signInWithEmailAndPassword(email, password)
         .then(user => {
-            dispatch({ type: 'LOGIN_USER_SUCCES', payload: user }); 
+            dispatch({ type: LOGIN_USER_SUCCES, payload: user }); 
         }); 
     };
 }; 
