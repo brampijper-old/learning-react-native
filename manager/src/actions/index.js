f91751159cd8f441003269a5f8767ee3af8f335e
bram <brampijper@gmail.com>
2018-02-05 12:13:56 +0100

    added helpfunctions for loginUserfail & succes

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -3,7 +3,8 @@ import firebase from 'firebase';
 import { 
         EMAIL_CHANGED, 
         PASSWORD_CHANGED,
-        LOGIN_USER_SUCCESS
+        LOGIN_USER_SUCCESS,
+        LOGIN_USER_FAIL
 } from './types';
 
 export const emailChanged = (text) => {
@@ -24,9 +25,25 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
     return (dispatch) => {
         firebase.auth().signInWithEmailAndPassword(email, password)
-        .then(user => {
-            dispatch({ type: LOGIN_USER_SUCCESS, payload: user }); 
-        }); 
+            .then(user => loginUserSucces(dispatch, user)) 
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .then(user => loginUserSucces(dispatch, user))
+                    .catch(() => loginUserFail(dispatch));
+            });
     };
 }; 
+
+const loginUserFail = (dispatch) => {
+    dispatch({
+        type: LOGIN_USER_FAIL
+    });
+};
+
+const loginUserSucces = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    });
+};
 
