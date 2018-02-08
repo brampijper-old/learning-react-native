152d50e62b95c32498b1e43f7e8e5168d0296794
bram <brampijper@gmail.com>
2018-02-08 16:48:48 +0100

    Implemented a flatlist for for displaying all the employees

--- manager/src/components/EmployeeList.js
+++ manager/src/components/EmployeeList.js
@@ -1,21 +1,37 @@
+import _ from 'lodash'; 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { FlatList } from 'react-native';
+import { employeeFetch } from '../actions';
+import EmployeeListItem from './EmployeeListItem';
 
 class EmployeeList extends Component {
+    componentDidMount() {
+        this.props.employeeFetch();
+    }
+
+    renderItem({ item }) {
+        return <EmployeeListItem employee={item} />;
+    }
+
     render() {
+        console.log(this.props);
         return (
-            <View>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-            </View>
+            <FlatList
+                data={this.props.employee}
+                renderItem={this.renderItem}
+            />
         );
     }
 }
 
-export default EmployeeList;
+const mapStateToProps = state => {
+    const employee = _.map(state.employee, (val, uid) => {
+        return { ...val, uid }; // { shift: 'Monday', name: 'S', id: '30303' };
+    }); 
+
+    return { employee }; 
+};
+
+export default connect(mapStateToProps, { employeeFetch })(EmployeeList);
 
