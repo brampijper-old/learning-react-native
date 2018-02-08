1c28445c8d2b7191965f22d2c02c4fe4a4237ee7
bram <brampijper@gmail.com>
2018-02-08 10:09:49 +0100

    Added employee create type

--- manager/src/actions/types.js
+++ manager/src/actions/types.js
@@ -6,3 +6,5 @@ export const LOGIN_USER_START = 'login_user_start';
 
 export const EMPLOYEE_UPDATE = 'employee_update';
 export const EMPLOYEE_CREATE = 'employee_create';
+export const EMPLOYEE_FETCH_SUCCES = 'employee_fetch_succes';
+
